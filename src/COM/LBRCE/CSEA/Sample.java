dcbb6352f277968cc4ebb065a8698daa7c9bcaef
Admin <Admin@DESKTOP-PTJ645C>
2023-09-21 14:02:14 +0530

    Team Lead Commit

---
+++ src/COM/LBRCE/CSEA/Sample.java
@@ -0,0 +1,16 @@
+package COM.LBRCE.CSEA;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+		raji();
+
+	}
+	public static void raji() {
+		// TODO Auto-generated method stub
+            System.out.println("Team Lead");
+	}
+
+
+}
